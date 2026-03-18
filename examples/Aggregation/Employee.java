5d0a985127d0bb6577f0e8b5a653a22cee437470
Maryam Skaik <mariamskaik2003@gmail.com>
2026-03-18 13:32:54 +0200

    Create Employee.java

---
+++ examples/Aggregation/Employee.java
@@ -0,0 +1,16 @@
+package Aggregation;
+
+/**
+ * Represents an Employee.
+ * 
+ * This class can exist independently
+ * → important for Aggregation concept
+ */
+public class Employee {
+
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+}
