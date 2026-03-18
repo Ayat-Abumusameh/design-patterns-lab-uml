5dc87c5ae6c0fa8dd4f2020f8e60157bf1dd6fc5
Maryam Skaik <mariamskaik2003@gmail.com>
2026-03-18 13:33:52 +0200

    Create Processor.java

---
+++ examples/Composition/Processor.java
@@ -0,0 +1,15 @@
+package Composition;
+
+/**
+ * Represents a Processor.
+ * 
+ * Used only within Computer
+ */
+public class Processor {
+
+    private String model;
+
+    public Processor(String model) {
+        this.model = model;
+    }
+}
