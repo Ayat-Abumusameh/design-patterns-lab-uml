1bdb5fd663757c60d48ea9920538c0e17ae44cd8
Maryam Skaik <mariamskaik2003@gmail.com>
2026-03-18 13:35:21 +0200

    Create Vehicle.java

---
+++ examples/Generalization/Vehicle.java
@@ -0,0 +1,16 @@
+package Generalization;
+
+/**
+ * Base class (Parent)
+ * 
+ * UML Concept: Generalization (Inheritance)
+ * - Common properties shared by subclasses
+ */
+public class Vehicle {
+
+    protected String brand;
+
+    public void start() {
+        System.out.println("Vehicle Starting");
+    }
+}
