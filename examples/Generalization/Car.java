6b66050490a7f23e7b17a4ff56cf339f8b8d41c5
Maryam Skaik <mariamskaik2003@gmail.com>
2026-03-18 13:35:43 +0200

    Create Car.java

---
+++ examples/Generalization/Car.java
@@ -0,0 +1,15 @@
+package Generalization;
+
+/**
+ * Child class (should extend Vehicle)
+ * 
+ * Demonstrates inheritance
+ */
+public class Car extends Vehicle {
+
+    private int doors;
+
+    public void openTrunk() {
+        System.out.println("Opening trunk");
+    }
+}
